48fc9f3b2953d5f75037ab2d51867b0cd3b31e5a
Ilya <deminiiii@gmail.com>
2020-10-16 20:23:58 +0300

    Revert "Correct style error - 'Leading .'"
    This reverts commit 8e5c0348e837fc6e313ba9174317ba8be1708466.

--- main.rb
+++ main.rb
@@ -1,20 +1,23 @@
 require_relative 'lib/console_interface'
 require_relative 'lib/game'
 
+# 1. Поздороваться
 puts "Всем привет!"
 
-# Загружаем случаное число из файла
+# 2. Загрузить случайное слово из файла
 word = File.readlines(__dir__ + '/data/words.txt', encoding: 'UTF-8', chomp: true).sample
 game = Game.new(word)
 console_interface = ConsoleInterface.new(game)
 
+# 3. Пока не закончилась игра
 until game.over?
-  # Выводим состояние игры
+  #   3.1. Вывести очередное состояние игры
   console_interface.print_out
-  # Спрашиваем букву у пользователя
+  #   3.2. Спросить очередную букву
   letter = console_interface.get_input
-  # Обновляем состояние игры
+  #   3.3. Обновить состояние игры
   game.play!(letter)
 end
 
+# 4. Вывести финальное состояние игры
 console_interface.print_out
